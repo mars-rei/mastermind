7fd2ffd35377f2b56cd31e3ff39064ca6f53c326
ninjangelo <lagdameoangelo@gmail.com>
2025-04-14 13:43:38 +0100

    created a potential structure for our game at the moment

--- main(DRAFT).py
+++ main(DRAFT).py
@@ -3,8 +3,10 @@ import sys, random
 from dataclasses import dataclass
 from enum import Enum
 from typing import Any, Callable, TypeAlias, Optional, NoReturn
+# ^^^^^^^^^^ IMPORTS ^^^^^^^^^^
 
 
+# Information
 """
 ---------- CMP5361 - Computer Mathematics and Declarative Programming ----------
 
@@ -16,41 +18,74 @@ Authors:
 
 """
 
+
+# ----- Interface Visuals -----
 mastermind_intro : str = """
--------------------------------------
-------------- MASTERMIND ------------
--------------------------------------
+ ___________________________________
+|     MASTERMIND - Command Line     |
+|___________________________________|
 """
 
-main_menu : str = """
+main_menu_options : str = """
 MAIN MENU ---------------------------
-(1) Single Player
-(2) Multiplayer
-(3) CAMPAIGN
+
+(0) Single Player
+(1) Multiplayer
+(2) Campaign
+(3) Exit
 """
 
-def display_mastermind_intro() -> None:
-    print(mastermind_intro)
-    print(main_menu)
 
-def recieve_main_menu_input() -> None:
+# ----- Program Flow and Functions -----
 
-    
+
+# TODO: parseMenuOption to deal with match-case instead of recieveMenuInput?
+def recieve_menu_input() -> None:
+    #DRAFT (as well)
     """
     i = int(input(">"))
-    if i == 1:
-        print("Choice 1!")
+    if i == 0:
+        print("SinglePlayer Mode")
+    elif i == 1:
+        print("Multiplayer Mode")
     elif i == 2:
-        print("Choice 2!")
-    elif i ==3:
-        print("Choice 3!")
+        print("Campaign Mode")
     else:
         print("Invalid Choice!")
     """
+        
+    #DRAFT for logic
+    try:
+        i = int(input(">"))
 
-def navigate_main_menu() -> None:
+        match i:
+            case 0:
+                # TODO - Document/Create start_single_player Function
+                print("SinglePlayer Mode")
+            case 1:
+                # TODO - Document/Create start_multiplayer Function
+                print("Multiplayer Mode")
+            case 2:
+                # TODO - Document/Create start_campaign Function
+                print("Campaign Mode")
+            case 3:
+                print("Exiting Mastermind...")
+                exit()
+    except ValueError:
+        print("ERROR: Invalid Input")
+
+
+# Prints Game Title & Main Menu
+def display_mastermind_intro() -> None:
+    print(mastermind_intro + main_menu_options)
+
+# Combines Interface display and Running Prompt
+def main_menu_navigation() -> None:
     display_mastermind_intro()
-    recieve_main_menu_input()
+
+    while True:
+        recieve_menu_input()
+
 
 if __name__=="__main__":
-    navigate_main_menu()
+    main_menu_navigation()
